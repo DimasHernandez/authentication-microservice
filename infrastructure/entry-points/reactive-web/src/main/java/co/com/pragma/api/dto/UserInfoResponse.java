3ec675dcb30ef016fb2531c9087c0d41da663b36
DimasHernandez <dimas.hernandez@utp.edu.co>
2025-08-26 11:34:21 -0500

    Successful implementation: obtaining a user through their identification document

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/dto/UserInfoResponse.java
@@ -0,0 +1,18 @@
+package co.com.pragma.api.dto;
+
+import co.com.pragma.model.user.enums.DocumentType;
+
+import java.util.UUID;
+
+public record UserInfoResponse(
+        UUID id,
+        String name,
+        String surname,
+        String email,
+        DocumentType documentType,
+        String documentNumber,
+        String address,
+        String phoneNumber,
+        String baseSalary
+) {
+}
