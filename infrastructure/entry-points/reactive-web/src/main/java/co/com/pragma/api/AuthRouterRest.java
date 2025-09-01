47a52334cd9193ed7e1e93210d12db75a0cd0920
DimasHernandez <dimas.hernandez@utp.edu.co>
2025-09-01 13:07:46 -0500

    Add documentation login endpoint

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/AuthRouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/AuthRouterRest.java
@@ -1,9 +1,19 @@
 package co.com.pragma.api;
 
 import co.com.pragma.api.config.AuthPath;
+import co.com.pragma.api.dto.LoginRequest;
+import co.com.pragma.api.dto.LoginResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -20,6 +30,37 @@ public class AuthRouterRest {
 
 
     @Bean
+    @RouterOperations({
+            @RouterOperation(
+                    path = "/api/v1/login",
+                    produces = {"application/json"},
+                    method = RequestMethod.POST,
+                    beanClass = AuthHandler.class,
+                    beanMethod = "listenLogin",
+                    operation = @Operation(
+                            operationId = "Create JWT token",
+                            summary = "Create token",
+                            description = "Create a token to verify the user's authenticity.",
+                            tags = {"Auth"},
+                            requestBody = @RequestBody(
+                                    required = true,
+                                    description = "Create Token",
+                                    content = @Content(schema = @Schema(implementation = LoginRequest.class))
+                            ),
+                            responses = {
+                                    @ApiResponse(responseCode = "201", description = "Token generated successfully",
+                                            content = @Content(schema = @Schema(implementation = LoginResponse.class))),
+                                    @ApiResponse(
+                                            responseCode = "422",
+                                            description = "Invalid credentials",
+                                            content = @Content(schema = @Schema(
+                                                    example = "{ \"error\": \"Error de validación\", \"code\": \"AUTH_001\", \"detail\": \"Credenciales invalidas\" }"
+                                            ))
+                                    )
+                            }
+                    )
+            )
+    })
     public RouterFunction<ServerResponse> authRouterFunction(AuthHandler authHandler) {
         return route(POST(authPath.getLogin()), this.authHandler::listenLogin);
     }
