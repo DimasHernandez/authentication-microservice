bba2b38c9b7aff2f3802ea94a90e0085181bab30
DimasHernandez <dimas.hernandez@utp.edu.co>
2025-09-07 19:54:02 -0500

    Refactoring error message in authentication when authorization or permissions fail

--- infrastructure/entry-points/security/src/main/java/co/com/pragma/security/SecurityConfig.java
+++ infrastructure/entry-points/security/src/main/java/co/com/pragma/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
             response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
 
             Map<String, Object> body = new HashMap<>();
-            body.put("status", HttpStatus.UNAUTHORIZED.value());
+            body.put("code", "AUTH_010");
             body.put("error", "No autorizado");
             body.put("message", "No tiene credenciales validas");
 
@@ -78,7 +78,7 @@ public class SecurityConfig {
             response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
 
             Map<String, Object> body = new HashMap<>();
-            body.put("status", HttpStatus.FORBIDDEN.value());
+            body.put("code", "AUTH_013");
             body.put("error", "Forbidden");
             body.put("message", "No tienes permisos para acceder a este recurso");
 
